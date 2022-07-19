840c3479b34a5bd70c03e445f9259b82c6911721
AsafeMaia123 <asafem37@gmail.com>
2022-07-19 20:28:06 -0300

    loja de varejo 2

---
+++ loja_de_varejo/src/View/list_of_providers.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listagem de fornecedores</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<nav class="bg-blue-400">
+            <ul>
+                <li class="inline">
+                    <a href="../../index.html">Home</a>
+                </li>
+                <li class="inline">
+                    <a href="form_add_product.php">Novo produto</a>
+                </li>
+                <li class="inline">
+                    <a href="form_add_provider.php">Novo fornecedor</a>
+                </li>
+                <li class="inline">
+                    <a href="../Controller/Produto.php?operation=list">Lista de produtos</a>
+                </li>
+                <li class="inline">
+                    <a href="#">Lista de fornecedores</a>
+                </li>
+            </ul>
+    </nav>
+    <h1 class="my-4 text-3xl font-bold text-center text-blue-800">Lista de fornecedores cadastrados</h1>
+    <table  class="m-auto">
+        <thead class="m-auto">
+            <th>#</th>
+            <th>Nome do fornecedor</th>
+            <th>CNPJ</th>
+            <th>Telefone do fornecedor</th>
+            <th>Logradouro</th>
+            <th>Nome</th>
+            <th>Nº</th>
+            <th>Complemento</th>
+            <th>Bairro</th>
+            <th>Cidade</th>
+            <th>CEP</th>
+
+        </thead>
+        <tbody>
+            <?php
+                session_start();
+                foreach($_SESSION['list_of_providers'] as $provider) :
+            ?>
+                <tr>
+                    <td>
+                        <?= $provider['provider_code'] ?>
+                    </td>
+                    <td>
+                        <?= $provider['provider_name'] ?>
+                    </td>
+                    <td>
+                        <?= $provider['cnpj'] ?>
+                    </td>
+                    <td>
+                        <?= $provider['provider_phone'] ?>
+                    </td>
+                    <td>
+                        <?= $provider['public_place'] ?>
+                    </td>
+                    <td>
+                        <?= $provider['street_name'] ?>
+                    </td>
+                    <td>
+                        <?= $provider['number_of_street'] ?>
+                    </td>
+                    <td>
+                        <?= $provider['complement'] ?>
+                    </td>
+                    <td>
+                        <?= $provider['neighborhood'] ?>
+                    </td>
+                    <td>
+                        <?= $provider['city'] ?>
+                    </td>
+                    <td>
+                        <?= $provider['zipcode'] ?>
+                    </td>
+                </tr>
+            <?php
+            endforeach;
+            ?>
+        </tbody>
+    </table>
+</body>
+</html>
